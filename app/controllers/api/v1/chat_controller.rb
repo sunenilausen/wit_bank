5b1072b4895eac20f66c2f0cd9667e37038a0c31
Sune Nilausen <sunenilausen@gmail.com>
2017-11-25 13:55:01 +0100

    New feature. access_token is in env variable

--- app/controllers/api/v1/chat_controller.rb
+++ app/controllers/api/v1/chat_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::ChatController < ApplicationController
 
   def message 
-
+    answer = params[:answer]
+    response = client.message(answer)
+    currency_type = response['entities']['currency'].first['value']
+    render json: {currency_type: currency_type}
   end
 
   private
     def client
-
+      Wit.new(access_token: ENV['wit_token'])
     end
 
 end
--- test/controllers/api/v1/chat_controller_test.rb
+++ test/controllers/api/v1/chat_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class Api::V1::ChatControllerTest < ActionDispatch::IntegrationTest
-  it 'should respond' do
-    get api_v1_chat_message_url 
-    assert_response :success
+  # it 'should respond' do
+  #   get api_v1_chat_message_url
+  #   assert_response :error
+  # end
+
+  it 'should respond with a question' do
+    get api_v1_chat_message_url params: {answer: 'I want Danish money', errand: 'withdrawal', question: 'currency_type'}
+
+    json_response = JSON.parse(response.body)
+
+    assert_equal 'DKK', json_response['currency_type']
   end
 end
