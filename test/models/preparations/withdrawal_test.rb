a8947ab941ef09c791f86e34d37cd0a56d4ae4ab
Sune Nilausen <sunenilausen@gmail.com>
2017-11-25 21:06:30 +0200

    Refactor: Removed unused stuff and added a test for complete code

--- test/models/preparations/withdrawal_test.rb
+++ test/models/preparations/withdrawal_test.rb
@@ -5,10 +5,18 @@ class Preparation::WithdrawalTest < ActiveSupport::TestCase
     withdrawal = Preparation::Withdrawal.new(answer: 'I want Danish money', field: 'currency')
 
     json = withdrawal.to_json
-    
+
     assert_equal 'currency', json[:field]
     assert_equal 'DKK', json[:value]
     assert_equal 'amount', json[:next_question]
+  end
+
+  it 'has nil value when inelligible' do
+    withdrawal = Preparation::Withdrawal.new(answer: 'jaldjg asdfasd', field: 'currency')
+
+    json = withdrawal.to_json
+    
+    assert_equal nil, json[:value]
   end
 
 end
